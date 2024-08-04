f8b5237f30178188e761ad1dbf4f586e4fe3e1de
Vaishnav-Nair <thisisvaishnavnair@gmail.com>
2024-08-04 12:56:27 +0530

    frontend done

---
+++ hand-drawing-app/frontend/src/components/VideoFeed.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function VideoFeed() {
+  return (
+    <div>
+      <h1>Hand Paint Application</h1>
+      <img src="http://127.0.0.1:5000/video_feed" alt="Video Feed" />
+    </div>
+  );
+}
+
+export default VideoFeed;
